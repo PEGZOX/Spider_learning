45ce52d5b89819d46104b115ff9bc0c41a8faa24
geralt <1751008821@qq.com>
2022-09-17 17:19:56 +0800

    #

---
+++ url_Batch_download.py
@@ -0,0 +1,38 @@
+import urllib.request  # url request
+import re  # regular expression
+import os  # dirs
+import time
+
+'''
+url 下载网址
+pattern 正则化的匹配关键词
+Directory 下载目录
+'''
+
+
+def BatchDownload(url, pattern, Directory):
+    opener = urllib.request.build_opener()
+
+
+    # 获取网页内容
+    content = opener.open(url).read().decode('utf8')
+
+    # 构造正则表达式，从content中匹配关键词pattern
+    raw_hrefs = re.findall(pattern, content, 0)
+
+    # set函数消除重复元素
+    hset = set(raw_hrefs)
+
+    # 下载链接
+    for href in hset:
+        filename = os.path.join(Directory, href.split('/')[-1])
+        print("正在下载", filename)
+        urllib.request.urlretrieve(href, filename)
+        print("成功下载！")
+
+        # 无sleep间隔，网站认定这种行为是攻击，反反爬虫
+        time.sleep(1)
+
+BatchDownload('http://www.cs.toronto.edu/~vmnih/data/mass_roads/train/sat/index.html',
+             'http.+?tiff',
+             'F:\常用遥感样本数据集\Road and Building Detection Datasets\Roads_Training_Set_Input_images')
